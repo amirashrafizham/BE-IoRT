297810e020cd4cf24a59ef391ed5c20d20f2ec78
Amir Ashraf Ahmad Izham <amirashrafizham@gmail.com>
2021-10-02 18:49:29 +0800

    Removed HTTPS redirection

--- Startup.cs
+++ Startup.cs
@@ -44,7 +44,7 @@ namespace BE_IoRT
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BE_IoRT v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
