07575ec7eb5fbb5c299fe27ce69ecd7ce25cca9c
Amir Ashraf Ahmad Izham <amirashrafizham@gmail.com>
2022-01-16 12:57:42 +0800

    feature: TurnRight, TurnLeft, ReverseRight, ReverseLeft

--- Services/RobotWheelService.cs
+++ Services/RobotWheelService.cs
@@ -40,14 +40,53 @@ namespace BE_IoRT.Services
             return RobotWheel;
         }
 
-        public Task<ActionResult<RobotWheel>> TurnRight(Speed speed, int time)
+        public async Task<ActionResult<RobotWheel>> TurnRight(Speed speed, int time)
         {
-            throw new System.NotImplementedException();
+            EncoderMotor motorLeft;
+            EncoderMotor motorRight;
+            var expansionPlate = PiTop4Board.Instance.GetOrCreateExpansionPlate();
+            motorLeft = expansionPlate.GetOrCreateEncoderMotor(EncoderMotorPort.M2);
+            motorRight = expansionPlate.GetOrCreateEncoderMotor(EncoderMotorPort.M3);
+
+            var RobotWheel = new RobotWheel()
+            {
+                MotorLeftDirection = motorLeft.ForwardDirection,
+                MotorRightDirection = motorRight.ForwardDirection
+            };
+
+            using (motorLeft)
+            {
+                motorLeft.SetSpeed(speed);
+                await Task.Delay(time);
+                motorLeft.Stop();
+            }
+
+            return RobotWheel;
         }
 
-        public Task<ActionResult<RobotWheel>> TurnLeft(Speed speed, int time)
+        public async Task<ActionResult<RobotWheel>> TurnLeft(Speed speed, int time)
         {
-            throw new System.NotImplementedException();
+            EncoderMotor motorLeft;
+            EncoderMotor motorRight;
+            var expansionPlate = PiTop4Board.Instance.GetOrCreateExpansionPlate();
+            motorLeft = expansionPlate.GetOrCreateEncoderMotor(EncoderMotorPort.M2);
+            motorRight = expansionPlate.GetOrCreateEncoderMotor(EncoderMotorPort.M3);
+            motorRight.ForwardDirection = ForwardDirection.CounterClockwise;
+
+            var RobotWheel = new RobotWheel()
+            {
+                MotorLeftDirection = motorLeft.ForwardDirection,
+                MotorRightDirection = motorRight.ForwardDirection
+            };
+
+            using (motorRight)
+            {
+                motorRight.SetSpeed(speed);
+                await Task.Delay(time);
+                motorRight.Stop();
+            }
+
+            return RobotWheel;
         }
 
         public async Task<ActionResult<RobotWheel>> Reverse(Speed speed, int time)
@@ -80,14 +119,53 @@ namespace BE_IoRT.Services
             return RobotWheel;
         }
 
-        public Task<ActionResult<RobotWheel>> ReverseRight(Speed speed, int time)
+        public async Task<ActionResult<RobotWheel>> ReverseRight(Speed speed, int time)
         {
-            throw new System.NotImplementedException();
+            EncoderMotor motorLeft;
+            EncoderMotor motorRight;
+            var expansionPlate = PiTop4Board.Instance.GetOrCreateExpansionPlate();
+            motorLeft = expansionPlate.GetOrCreateEncoderMotor(EncoderMotorPort.M2);
+            motorRight = expansionPlate.GetOrCreateEncoderMotor(EncoderMotorPort.M3);
+
+            var RobotWheel = new RobotWheel()
+            {
+                MotorLeftDirection = motorLeft.ForwardDirection,
+                MotorRightDirection = motorRight.ForwardDirection
+            };
+
+            using (motorRight)
+            {
+                motorRight.SetSpeed(speed);
+                await Task.Delay(time);
+                motorRight.Stop();
+            }
+
+            return RobotWheel;
         }
 
-        public Task<ActionResult<RobotWheel>> ReverseLeft(Speed speed, int time)
+        public async Task<ActionResult<RobotWheel>> ReverseLeft(Speed speed, int time)
         {
-            throw new System.NotImplementedException();
+            EncoderMotor motorLeft;
+            EncoderMotor motorRight;
+            var expansionPlate = PiTop4Board.Instance.GetOrCreateExpansionPlate();
+            motorLeft = expansionPlate.GetOrCreateEncoderMotor(EncoderMotorPort.M2);
+            motorRight = expansionPlate.GetOrCreateEncoderMotor(EncoderMotorPort.M3);
+            motorLeft.ForwardDirection = ForwardDirection.CounterClockwise;
+
+            var RobotWheel = new RobotWheel()
+            {
+                MotorLeftDirection = motorLeft.ForwardDirection,
+                MotorRightDirection = motorRight.ForwardDirection
+            };
+
+            using (motorLeft)
+            {
+                motorRight.SetSpeed(speed);
+                await Task.Delay(time);
+                motorRight.Stop();
+            }
+
+            return RobotWheel;
         }
     }
 }
